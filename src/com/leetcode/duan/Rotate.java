2afe35ee2ecd1776a5fb4f74b72159a438ebbf55
duan <18296127105@163.com>
2019-12-14 23:49:16 +0800

    旋转数组
    Signed-off-by: duan <18296127105@163.com>

---
+++ src/com/leetcode/duan/Rotate.java
@@ -0,0 +1,24 @@
+package com.leetcode.duan;
+
+import java.util.Arrays;
+
+public class Rotate {
+    public static void main(String[] args) {
+        int[] nums = new int[] {1,2,3,4,5,6,7};
+        rotate(nums, 10);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    private static void rotate(int[] nums, int k) {
+        int length = nums.length;
+        //循环大于一轮的情况下取余数
+        k = k % length;
+        for(int i = 1; i<= k; i++){
+            int temp = nums[length - 1];
+            for(int j = length - 1; j > 0; j--){
+                nums[j] = nums[j-1];
+            }
+            nums[0] = temp;
+        }
+    }
+}
